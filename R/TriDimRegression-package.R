a450c557551cfaf8fecead89ee884f862b77bb68
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-09 21:07:02 +0200

    Package description and dependences

---
+++ R/TriDimRegression-package.R
@@ -0,0 +1,20 @@
+#' The 'TriDimRegression' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name TriDimRegression-package
+#' @aliases TriDimRegression
+#' @useDynLib TriDimRegression, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom dplyr bind_cols bind_rows
+#' @importFrom future availableCores
+#' @importFrom glue glue
+#' @importFrom purrr map map_df
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
