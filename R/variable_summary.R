bbf90eb07ce199e3fb4cb606edcab1cc18efdcc3
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-10 11:05:38 +0200

    Sanity checks + compatible with a vector via explicit conversion to matrix

--- R/variable_summary.R
+++ R/variable_summary.R
@@ -6,15 +6,20 @@
 #' @param probs A numeric vector of quantiles to compute.
 #'
 #' @return data.frame with "{var_name}" column for the mean and
-#' "dv{var_name}_{prob}" columns for each probability.
+#' "{var_name}_{prob}" columns for each probability.
 #' @keywords internal
 #' @export
 variable_summary <- function(var_name, var_matrix, probs){
+  # sanity checks
+  stopifnot(is.character(var_name), !is.na(var_name))
+  stopifnot(sum(is.finite(var_matrix)) == length(var_matrix))
+  stopifnot(is.numeric(probs) || is.null(probs))
+
   var_summary <- list()
-  var_summary[[glue::glue("{var_name}")]] <- apply(var_matrix, MARGIN=2, FUN=mean)
+  var_summary[[glue::glue("{var_name}")]] <- apply(as.matrix(var_matrix), MARGIN=2, FUN=mean)
   if (is.null(probs)) return(as.data.frame(var_summary))
 
-  computed_probs <- purrr::map(probs, ~apply(var_matrix, MARGIN=2, FUN=quantile, probs=.))
+  computed_probs <- purrr::map(probs, ~apply(as.matrix(var_matrix), MARGIN=2, FUN=quantile, probs=.))
   names(computed_probs) <- glue::glue("{var_name}_{probs*100}")
   as.data.frame(c(var_summary, computed_probs))
 }
