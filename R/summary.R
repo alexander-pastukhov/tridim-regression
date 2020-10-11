7dc97b41ee5a120e96755d111f80d11f1a1fe5df
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-11 12:20:54 +0200

    Fit summary

---
+++ R/summary.R
@@ -0,0 +1,27 @@
+#' Summary for a tridim_transform object
+#'
+#' @param object A tridim_transform object
+#'
+#' @export
+#'
+#' @examples
+#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#'                                      data = NakayaData,
+#'                                      transformation = 'euclidean')
+#' summary(euc2)
+
+summary.tridim_transform <- function(object){
+  cat('Call: ')
+  cat(deparse(object$formula))
+  cat('\n')
+
+  cat(glue::glue('Data dimensions: {object$dimN}\n\n'))
+  cat(glue::glue('Transformation: {object$transformation}\n\n'))
+  theR2 <- round(R2(object, adjust = FALSE), 3)
+  cat(glue::glue('R2: {theR2[1,1]} [{theR2[1,2]}..{theR2[1,3]}]\n\n'))
+  theAdjR2 <- round(R2(object, adjust = TRUE), 3)
+  cat(glue::glue('Adjusted R2: {theAdjR2[1,1]} [{theAdjR2[1,2]}..{theAdjR2[1,3]}]\n\n'))
+
+  cat(glue::glue('\n\nCoefficients:\n\n'))
+  print(coef(object, summary=TRUE), row.names = FALSE)
+}
