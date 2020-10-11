2ce20c321db1783b7c070ee63c7f065e5d349bc2
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-11 12:02:15 +0200

    Basic plot

---
+++ R/plot.R
@@ -0,0 +1,23 @@
+#' Posterior interval plots for key parameters. Uses bayesplot::mcmc_intervals.
+#'
+#' @param object A tridim_transform object
+#' @param ... Extra parameters to be passed to bayesplot::mcmc_intervals
+#'
+#' @export
+#'
+#' @examples
+#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#'                                         data = NakayaData,
+#'                                         transformation = 'euclidean')
+#' plot(euc2)
+plot.tridim_transform <- function(object, ...){
+  if (object$dimN == 2){
+    # bidimensional regression
+    coef_names <- c("scale", "shear", "rotation", "tilt", "translation")
+  }
+  else {
+    coef_names <- c("rotation", "scale", "translation", "shearX", "shearY", "shearZ")
+  }
+
+  bayesplot::mcmc_intervals(as.matrix(object$stanfit, pars = coef_names))
+}
