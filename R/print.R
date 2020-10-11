1295b09f83efff0a3eff5eef5b58d6d88ac517d6
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-11 12:02:26 +0200

    Basic print

---
+++ R/print.R
@@ -0,0 +1,24 @@
+#' Prints out tridim_transform object
+#'
+#' @param object A tridim_transform object
+#'
+#' @export
+#'
+#' @examples
+#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#'                                         data = NakayaData,
+#'                                         transformation = 'euclidean')
+#' print(euc2)
+
+print.tridim_transform <- function(object){
+  cat('Call: ')
+  cat(deparse(object$formula))
+  cat('\n')
+
+  cat(glue::glue('Data dimensions: {object$dimN}\n\n'))
+  cat(glue::glue('Transformation: {object$transformation}\n\n'))
+
+  object_coefs <-
+  printCoefmat(tidyr::pivot_wider(coef(object, summary=TRUE, probs = NULL),
+                                  names_from="Coef", values_from="Mean"))
+}
