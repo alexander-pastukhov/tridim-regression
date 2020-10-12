c7fdd22c90502abf686f6a03e785414756eb7edb
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-12 12:03:03 +0200

    Removed invalid examples

--- R/check_variables.R
+++ R/check_variables.R
@@ -15,11 +15,6 @@
 #' @examples
 #' # This should produce no error messages
 #' check_variables(matrix(c(1, 2, 3, 4), ncol=2), "test matrix")
-#' # This should produce non-numeric data error message
-#' check_variables(matrix(as.character(c(1, 2, 3, 4)), ncol=2), "test matrix")
-#' # These should produce non-finite data error message
-#' check_variables(matrix(c(1, 2, NA, 4), ncol=2), "test matrix")
-#' check_variables(matrix(c(1, 2, Inf, 4), ncol=2), "test matrix")
 check_variables <- function(var, var_label){
   if (!is.numeric(var)) stop(sprintf("%s is not a numeric", var_label))
   if (sum(!is.finite(var)) > 0) stop(sprintf("%s has non-finite elements", var_label))
