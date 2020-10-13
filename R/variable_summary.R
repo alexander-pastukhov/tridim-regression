d7d440633431f0fabc3afc599f67053a72422010
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-13 13:21:53 +0200

    No square brackets in docs, example

--- R/variable_summary.R
+++ R/variable_summary.R
@@ -2,13 +2,14 @@
 #' variable.
 #'
 #' @param var_name String, name of the dependent variable
-#' @param var_matrix Numeric matrix [samplesN, observationsN].
+#' @param var_matrix Numeric matrix samplesN x observationsN.
 #' @param probs A numeric vector of quantiles to compute.
 #'
 #' @return data.frame with "{var_name}" column for the mean and
 #' "{var_name}_{prob}" columns for each probability.
 #' @keywords internal
 #' @export
+#' @examples variable_summary("test", matrix(1:1000, ncol = 1), c(0.05, 0.95))
 variable_summary <- function(var_name, var_matrix, probs){
   # sanity checks
   stopifnot(is.character(var_name), !is.na(var_name))
