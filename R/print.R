f426d21824d05e8c1cc8c0c5cbb531b094c22304
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-13 13:20:32 +0200

    dontrun, no setMethod

--- R/print.R
+++ R/print.R
@@ -5,10 +5,12 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
 #'                                      data = NakayaData,
 #'                                      transformation = 'euclidean')
 #' euc2
+#' }
 
 print.tridim_transform <- function(object){
   cat('Call: ')
@@ -18,6 +20,8 @@ print.tridim_transform <- function(object){
   cat(glue::glue('Data dimensions: {object$dimN}\n\n'))
   cat(glue::glue('Transformation: {object$transformation}\n\n'))
   cat(glue::glue('\n\nCoefficients:\n\n'))
-  printCoefmat(tidyr::pivot_wider(TriDimRegression::coef(object, summary=TRUE, probs = NULL),
+  printCoefmat(tidyr::pivot_wider(coef.tridim_transform(object, summary=TRUE, probs = NULL),
                                   names_from="Coef", values_from="Mean"))
 }
+
+# setMethod("print", "tridim_transform", print.tridim_transform)
