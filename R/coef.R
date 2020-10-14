4af759c63fa468e348a95e1c4f92db605dbbad5b
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-10-14 17:26:31 +0200

    Correct use of generics

--- R/coef.R
+++ R/coef.R
@@ -1,11 +1,11 @@
 #' Posterior distributions for transformation coefficients
 #' in full or summarized form.
 #'
-#' @param object An object of class "tridim_transform".
+#' @param object An object of class [tridim_transform][tridim_transform-class()].
 #' @param summary Whether summary statistics should be returned instead of
 #' raw sample values. Defaults to \code{TRUE}
-#' @param probs The percentiles used to compute summary, defaults to 89% credible inteval.
-#'
+#' @param probs The percentiles used to compute summary, defaults to 89% credible interval.
+#' @param ... Unused
 #'
 #' @return If summary=FALSE, a list with matrix iterationsN x dimensionsN for
 #' each variable.
@@ -26,7 +26,7 @@
 #' # coefficients' summary with 89% CI
 #' coef(euc2)
 #' }
-coef.tridim_transform <- function(object, summary=TRUE,  probs=c(0.055, 0.945)){
+coef.tridim_transform <- function(object, summary=TRUE,  probs=c(0.055, 0.945), ...){
   if (object$dimN == 2){
     # bidimensional regression
     coef_names <- c("scale", "shear", "rotation", "tilt", "translation")
@@ -40,5 +40,5 @@ coef.tridim_transform <- function(object, summary=TRUE,  probs=c(0.055, 0.945)){
     return(coef_samples)
   }
 
-  purrr::map_df(names(coef_samples), ~TriDimRegression::coef_summary(., coef_samples[[.]], probs))
+  purrr::map_df(names(coef_samples), ~coef_summary(., coef_samples[[.]], probs))
 }
