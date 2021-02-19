f4967119531e221a3b680cfd8cf7f04537e0f175
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-02-19 10:54:48 +0100

    Check for exponential prior and test

--- tests/testthat/test-check_priors.R
+++ tests/testthat/test-check_priors.R
@@ -13,4 +13,18 @@ test_that("Priors check", {
   # Error: non-positive variance
   expect_error(check_normal_prior(c(0, -2), "scale"))
   expect_error(check_normal_prior(c(0, 0), "scale"))
+
+
+  # This should produce no error messages
+  expect_equal(check_exponential_prior(2, "sigma"), expected=NULL)
+
+  # Error: non-numeric data
+  expect_error(check_exponential_prior("0", "sigma"))
+
+  # Error: wrong number of values
+  expect_error(check_exponential_prior(c(0, 2, 3), "sigma"))
+
+  # Error: non-positive value
+  expect_error(check_exponential_prior(0, "sigma"))
+  expect_error(check_exponential_prior(-1, "sigma"))
 })
