f776a5ca8dfc002baa44d0cd5c4f3bff1bcfa515
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-18 15:38:31 +0100

    Explicit a and b names to make checking easier

--- R/bi_transformation_matrices.R
+++ R/bi_transformation_matrices.R
@@ -1,6 +1,6 @@
 #' 2D Translation Matrix
 #'
-#' @param params numeric, coefficients a1 and a2
+#' @param a numeric, coefficients a1 and a2
 #'
 #' @return matrix 3x3
 #' @export
@@ -16,46 +16,49 @@ m2_translation <- function(a){
 
 #' 2D Euclidean
 #'
-#' @param params numeric, coefficients a1, a2, b1, b2
+#' @param a numeric, coefficients a1, a2, b1, b2
 #'
 #' @return matrix 3x3
 #' @export
 #' @examples
 #' m2_euclidean(c(2, 3, 1, 0.5))
-m2_euclidean <- function(p){
-  matrix(c(p[2 + 1],-p[2 + 2], 0,
-           p[2 + 2], p[2 + 1], 0,
-           p[1],     p[2],     1),
+m2_euclidean <- function(a){
+  b <- a[c(-1, -2)]
+  matrix(c(b[1],-b[2], 0,
+           b[2], b[1], 0,
+           a[1], a[2], 1),
          ncol=3, byrow=TRUE)
 }
 
 #' 2D Affine
 #'
-#' @param params numeric, coefficients a1, a2, b1...b4
+#' @param a numeric, coefficients a1, a2, b1...b4
 #'
 #' @return matrix 3x3
 #' @export
 #' @examples
 #' m2_affine(c(2, 3, 1, 0.5, 1, 5))
-m2_affine <- function(p){
-  matrix(c(p[2 + 1], p[2 + 3], 0,
-           p[2 + 2], p[2 + 4], 0,
-           p[1],     p[2],     1),
+m2_affine <- function(a){
+  b <- a[c(-1, -2)]
+  matrix(c(b[1], b[3], 0,
+           b[2], b[4], 0,
+           a[1], a[2], 1),
          ncol=3, byrow=TRUE)
 }
 
 
 #' 2D Projective
 #'
-#' @param params numeric, coefficients a1, a2, b1...b6
+#' @param a numeric, coefficients a1, a2, b1...b6
 #'
 #' @return matrix 3x3
 #' @export
 #' @examples
 #' m2_projective(c(2, 3, 1, 0.5, 1, 5, 2, 4))
-m2_projective <- function(p){
-  matrix(c(p[2 + 1], p[2 + 3], p[2 + 5],
-           p[2 + 2], p[2 + 4], p[2 + 6],
-           p[1],     p[2],     1),
+m2_projective <- function(a){
+  b <- a[c(-1, -2)]
+  matrix(c(b[1], b[3], b[5],
+           b[2], b[4], b[6],
+           a[1], a[2], 1),
          ncol=3, byrow=TRUE)
 }
