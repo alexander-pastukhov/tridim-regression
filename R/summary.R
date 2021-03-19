68804c842936678f0d227e973aa55f738930683c
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-19 10:54:32 +0100

    No adjusted R2

--- R/summary.R
+++ R/summary.R
@@ -1,19 +1,19 @@
-#' Summary for a tridim_transform object
+#' Summary for a tridim_transformation object
 #'
-#' @param object A [tridim_transform][tridim_transform-class()] object
+#' @param object A [tridim_transformation][tridim_transformation-class()] object
 #' @param ... Unused
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
-#'                                      data = NakayaData,
-#'                                      transformation = 'euclidean')
+#' euc2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
+#'                            data = NakayaData,
+#'                            transformation = 'euclidean')
 #' summary(euc2)
 #' }
 
-summary.tridim_transform <- function(object, ...){
+summary.tridim_transformation <- function(object, ...){
   cat('Call: ')
   cat(deparse(object$formula))
   cat('\n')
@@ -21,12 +21,10 @@ summary.tridim_transform <- function(object, ...){
   cat(glue::glue('Data dimensions: {object$dimN}\n\n'))
   cat(glue::glue('Transformation: {object$transformation}\n\n'))
   theR2 <- round(TriDimRegression::R2(object, adjust = FALSE), 3)
-  cat(glue::glue('R2: {theR2[1,1]} [{theR2[1,2]}..{theR2[1,3]}]\n\n'))
-  theAdjR2 <- round(TriDimRegression::R2(object, adjust = TRUE), 3)
-  cat(glue::glue('Adjusted R2: {theAdjR2[1,1]} [{theAdjR2[1,2]}..{theAdjR2[1,3]}]\n\n'))
+  cat(glue::glue('Bayesian R2: {theR2[1,1]} [{theR2[1,2]}..{theR2[1,3]}]\n\n'))
 
   cat(glue::glue('\n\nCoefficients:\n\n'))
-  print(coef.tridim_transform(object, summary=TRUE), row.names = FALSE)
+  print(coef.tridim_transformation(object, summary=TRUE), row.names = FALSE)
 }
 
 # setMethod("summary", "tridim_transform", summary.tridim_transform)
