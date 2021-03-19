a31ff641bd08d33199b7f9c3d285a2e9f366b5b8
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-19 13:20:43 +0100

    Updated stan files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,12 +7,10 @@
 using namespace Rcpp;
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4bi_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4tri_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4tridim_transformation_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4bi_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bi_mod, 0},
-    {"_rcpp_module_boot_stan_fit4tri_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4tri_mod, 0},
+    {"_rcpp_module_boot_stan_fit4tridim_transformation_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4tridim_transformation_mod, 0},
     {NULL, NULL, 0}
 };
 
--- src/stanExports_tridim_transformation.cc
+++ src/stanExports_tridim_transformation.cc
@@ -2,12 +2,12 @@
 
 #include <Rcpp.h>
 using namespace Rcpp ;
-#include "stanExports_tri.h"
+#include "stanExports_tridim_transformation.h"
 
-RCPP_MODULE(stan_fit4tri_mod) {
+RCPP_MODULE(stan_fit4tridim_transformation_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_tri")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_tridim_transformation")
 
     .constructor<SEXP,SEXP,SEXP>()
 
