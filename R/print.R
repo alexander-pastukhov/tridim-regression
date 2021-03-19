4c0c42a924f7f586f69964f199de74043d8912e8
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-19 10:57:14 +0100

    Uses transformATION

--- R/print.R
+++ R/print.R
@@ -1,19 +1,19 @@
-#' Prints out tridim_transform object
+#' Prints out tridim_transformation object
 #'
-#' @param x A [tridim_transform][tridim_transform-class()] object
+#' @param x A [tridim_transformation][tridim_transformation-class()] object
 #' @param ... Unused
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
-#'                                      data = NakayaData,
-#'                                      transformation = 'euclidean')
+#' euc2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
+#'                            data = NakayaData,
+#'                            transformation = 'euclidean')
 #' euc2
 #' }
 
-print.tridim_transform <- function(x, ...){
+print.tridim_transformation <- function(x, ...){
   cat('Call: ')
   cat(deparse(x$formula))
   cat('\n')
@@ -21,7 +21,7 @@ print.tridim_transform <- function(x, ...){
   cat(glue::glue('Data dimensions: {x$dimN}\n\n'))
   cat(glue::glue('Transformation: {x$transformation}\n\n'))
   cat(glue::glue('\n\nCoefficients:\n\n'))
-  printCoefmat(tidyr::pivot_wider(coef.tridim_transform(x, summary=TRUE, probs = NULL),
+  printCoefmat(tidyr::pivot_wider(coef.tridim_transformation(x, summary=TRUE, probs = NULL),
                                   names_from="Coef", values_from="Mean"))
 }
 
