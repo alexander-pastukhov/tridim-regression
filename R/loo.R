ae22dca4419ecf4992ab09d26e33ddf7d37bb8f5
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-19 10:54:23 +0100

    transformATION

--- R/loo.R
+++ R/loo.R
@@ -2,19 +2,19 @@
 #' cross-validation via loo library. It can be used
 #' for a model comparison via loo::loo_compare() function.
 #'
-#' @param object A [tridim_transform][tridim_transform-class()] object
+#' @param object A [tridim_transformation][tridim_transformation-class()] object
 #'
 #' @return A named list, see [loo::loo()] for details.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#' euc2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
 #'   NakayaData, transformation = 'euclidean')
-#' aff2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#' aff2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
 #'   NakayaData, transformation = 'affine')
 #' loo::loo_compare(loo(euc2), loo(aff2))
 #' }
-loo.tridim_transform <- function(object) {
+loo.tridim_transformation <- function(object) {
   rstan::loo(object$stanfit)
 }
--- R/waic.R
+++ R/waic.R
@@ -5,20 +5,20 @@
 #' via [loo][loo-package] library. It can be used for a model comparison via
 #' [loo::loo_compare()][loo::loo_compare] function.
 #'
-#' @param object A [tridim_transform][tridim_transform-class()] object
+#' @param object A [tridim_transformation[[tridim_transformation-class()] object
 #'
 #' @return A named list, see [loo::waic()] for details.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' euc2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#' euc2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
 #'   NakayaData, transformation = 'euclidean')
-#' aff2 <- fit_geometric_transformation(depV1+depV2~indepV1+indepV2,
+#' aff2 <- fit_transformation(depV1+depV2~indepV1+indepV2,
 #'   NakayaData, transformation = 'affine')
 #' loo::loo_compare(waic(euc2), waic(aff2))
 #' }
-waic.tridim_transform <- function(object) {
+waic.tridim_transformation <- function(object) {
   log_lik <- loo::extract_log_lik(object$stanfit, "log_lik", merge_chains = FALSE)
   loo::waic(log_lik)
 }
