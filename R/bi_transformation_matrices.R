7df59b6bf7a81e678b8544cd3947cdd671b343f0
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-03-22 12:55:13 +0100

    Input checks

--- R/bi_transformation_matrices.R
+++ R/bi_transformation_matrices.R
@@ -1,7 +1,7 @@
 #' 2D Translation Matrix
 #'
-#' @param a numeric, translation
-#' @param b numeric, all other coefficients
+#' @param a numeric, 2: translation
+#' @param b numeric, ignored
 #'
 #' @return matrix 3x3
 #' @export
@@ -9,7 +9,8 @@
 #'
 #' @examples
 #' m2_translation(c(2, 3))
-m2_translation <- function(a, b){
+m2_translation <- function(a, b=NULL){
+  stopifnot(is.finite(a) && length(a)==2)
   matrix(c(1,    0,    0,
            0,    1,    0,
            a[1], a[2], 1),
@@ -18,8 +19,8 @@ m2_translation <- function(a, b){
 
 #' 2D Euclidean
 #'
-#' @param a numeric, translation
-#' @param b numeric, all other coefficients
+#' @param a numeric, 2: translation
+#' @param b numeric, 2: all other coefficients
 #'
 #' @return matrix 3x3
 #' @keywords internal
@@ -27,6 +28,7 @@ m2_translation <- function(a, b){
 #' @examples
 #' m2_euclidean(c(2, 3), c(1, 0.5))
 m2_euclidean <- function(a, b){
+  stopifnot(is.finite(a) && length(a)==2 && is.finite(b) && length(b)==2)
   matrix(c(b[1],-b[2], 0,
            b[2], b[1], 0,
            a[1], a[2], 1),
@@ -35,8 +37,8 @@ m2_euclidean <- function(a, b){
 
 #' 2D Affine
 #'
-#' @param a numeric, translation
-#' @param b numeric, all other coefficients
+#' @param a numeric, 2: translation
+#' @param b numeric, 4: all other coefficients
 #'
 #' @return matrix 3x3
 #' @keywords internal
@@ -44,6 +46,7 @@ m2_euclidean <- function(a, b){
 #' @examples
 #' m2_affine(c(2, 3), c(1, 0.5, 1, 5))
 m2_affine <- function(a, b){
+  stopifnot(is.finite(a) && length(a)==2 && is.finite(b) && length(b)==4)
   matrix(c(b[1], b[3], 0,
            b[2], b[4], 0,
            a[1], a[2], 1),
@@ -53,8 +56,8 @@ m2_affine <- function(a, b){
 
 #' 2D Projective
 #'
-#' @param a numeric, translation
-#' @param b numeric, all other coefficients
+#' @param a numeric, 2: translation
+#' @param b numeric, 6: all other coefficients
 #'
 #' @return matrix 3x3
 #' @keywords internal
@@ -62,6 +65,7 @@ m2_affine <- function(a, b){
 #' @examples
 #' m2_projective(c(2, 3), c(1, 0.5, 1, 5, 2, 4))
 m2_projective <- function(a, b){
+  stopifnot(is.finite(a) && length(a)==2 && is.finite(b) && length(b)==6)
   matrix(c(b[1], b[3], b[5],
            b[2], b[4], b[6],
            a[1], a[2], 1),
